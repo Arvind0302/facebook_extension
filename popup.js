bab4258a244920a385e5c5d0b8eb5b8f5c737886
Arvind0302 <arvind.yadav.mec20@itbhu.ac.in>
2024-06-07 09:48:16 +0530

    scrolling done

--- popup.js
+++ popup.js
@@ -47,12 +47,42 @@ document.getElementById("fetchData").addEventListener("click", () => {
 
 
 
-function fetchAllFacebookData() {
+async function fetchAllFacebookData() {
   let friends = [];
-  const container = document.querySelector('div[aria-label="All friends"][role="navigation"]>div>div:nth-child(2)>div:nth-child(1)>div:nth-child(2)>div');
+  const container = document.querySelector('div[aria-label="All friends"][role="navigation"]>div>div:nth-child(2)');
   console.log("container", container);
 
-  // scroll to bottom of container to load all data
+  async function scrollPage() {
+    await new Promise((resolve) => {
+      let totalHeight = 0;
+      const distance = 500;
+      const scrollInterval = setInterval(() => {
+        const scrollHeight = container.scrollHeight;
+        container.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= scrollHeight) {
+          console.log("break from scrollPage")
+          clearInterval(scrollInterval);
+          resolve();
+        }
+      }, 1000);
+    });
+  }
+
+  async function scrollAndWait() {
+    while (true) {
+      const beforeScroll = container.scrollY;
+      await scrollPage();
+      const afterScroll = container.scrollY;
+      if (beforeScroll === afterScroll) {
+        console.log("break", beforeScroll, afterScroll);
+        break; 
+      }
+    }
+  }
+
+  await scrollAndWait();
+
 
   const data = container.querySelectorAll('div[data-visualcompletion="ignore-dynamic"]');
   for (let i = 0; i < data.length; i++) {
@@ -63,15 +93,10 @@ function fetchAllFacebookData() {
       const url = anchorTag.getAttribute("href");
       const name = spanElements[2].innerText;
       const mutual = spanElements.length==6 ? spanElements[5].innerText : "No mutual friends";
-      console.log(url, name, mutual);
       friends.push({name, url, mutual});
     }
-    else
-  {
-    console.log("account deactivated");
-  }
-
     }
+    console.log("friends Data",friends);
   return friends;
 }
 
