53c639657a4a47c5eae0f4390874212a89f57c85
Arvind0302 <arvind.yadav.mec20@itbhu.ac.in>
2024-06-07 10:15:39 +0530

    updated readme and added comments

--- README.md
+++ README.md
@@ -2,4 +2,27 @@
 
 This Chrome extension allows you to extract and view data of your Facebook friends directly from the Facebook Friends List page.
 
+## Features
+
+- Extract Friends Data: Extracts data such as friend's name, profile URL, and mutual friends from your Facebook Friends List.
+- Error Handling: Provides error messages if the extension is used on unsupported pages or encounters issues.
+- Infinite Scrolling Support: Handles infinite scrolling to ensure all friends data is loaded before  extraction.
+
+## Usage
+
+- Install the extension from the Chrome Web Store.
+- Navigate to your Facebook Friends List page.
+- Click on the extension icon in the toolbar.
+- The extension will extract the friend data and display it on the popup.
+
+## Installation
+
+- Clone this repository or download the ZIP file.
+- Open Chrome and navigate to chrome://extensions/.
+- Enable Developer mode.
+- Click on "Load unpacked" and select the extension directory.
+
+### Result will look like this : 
+
 ![image](https://github.com/Arvind0302/facebook_extension/assets/82073257/43cca463-9e95-4548-ac6c-b3be7f57bf56)
+
--- popup.js
+++ popup.js
@@ -1,57 +1,59 @@
-const output = document.getElementById('output');
+const output = document.getElementById("output");
+// added eventListener to button to scrape data
 document.getElementById("fetchData").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs && tabs.length > 0) {
       const url = new URL(tabs[0].url);
-      if (url.hostname.includes('facebook.com')) {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabs[0].id },
-          func: fetchAllFacebookData
-        },
-        (results) => {
-          output.innerHTML = '';
-          if (results && results.length > 0) {
-            const friends = results[0].result;
-            friends?.forEach(friend => {
-              const li = document.createElement('li');
-              const a = document.createElement('a');
-              a.href = friend.url;
-              a.textContent = friend.name;
-              a.target = '_blank'; 
-              const span = document.createElement('span');
-              span.textContent = friend.mutual;
-              li.appendChild(a);
-              li.appendChild(document.createTextNode(' - '));
-              li.appendChild(span);
+      if (url.hostname.includes("facebook.com")) {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tabs[0].id },
+            func: fetchAllFacebookData,
+          },
+          (results) => {
+            // make a list of returned array and populate the list in HTML
+            output.innerHTML = "";
+            if (results && results.length > 0) {
+              const friends = results[0].result;
+              friends?.forEach((friend) => {
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.href = friend.url;
+                a.textContent = friend.name;
+                a.target = "_blank";
+                const span = document.createElement("span");
+                span.textContent = friend.mutual;
+                li.appendChild(a);
+                li.appendChild(document.createTextNode(" - "));
+                li.appendChild(span);
+                output.appendChild(li);
+              });
+            } else {
+              const li = document.createElement("li");
+              li.textContent = "No friends found or unable to fetch data.";
               output.appendChild(li);
-            });
-          } else {
-            const li = document.createElement('li');
-            li.textContent = 'No friends found or unable to fetch data.';
-            output.appendChild(li);
+            }
           }
-        }
-      );
+        );
+      } else {
+        output.innerHTML =
+          "<li>This extension can only be used on Facebook pages.</li>";
+      }
     } else {
-      output.innerHTML = '<li>This extension can only be used on Facebook pages.</li>';
+      console.error("No active tab found");
     }
-  }
-  else
-  {
-    console.error("No active tab found");
-
-  }
   });
 });
 
-
-
 async function fetchAllFacebookData() {
   let friends = [];
-  const container = document.querySelector('div[aria-label="All friends"][role="navigation"]>div>div:nth-child(2)');
+  const container = document.querySelector(
+    'div[aria-label="All friends"][role="navigation"]>div>div:nth-child(2)'
+  );
   console.log("container", container);
   console.log("before scrolling", container.scrollTop);
+
+  // function to scroll page 
   async function scrollPage() {
     await new Promise((resolve) => {
       let totalHeight = 0;
@@ -68,13 +70,14 @@ async function fetchAllFacebookData() {
     });
   }
 
+  // wait until data is being loaded and then scroll again
   async function scrollAndWait() {
     while (true) {
       const beforeScroll = container.scrollY;
       await scrollPage();
       const afterScroll = container.scrollY;
       if (beforeScroll === afterScroll) {
-        break; 
+        break;
       }
     }
   }
@@ -83,19 +86,23 @@ async function fetchAllFacebookData() {
 
   console.log("after scrolling", container.scrollTop);
 
-  const data = document.querySelectorAll('div[data-visualcompletion="ignore-dynamic"]');
+  // scrape the required data from HTML elements
+  const data = document.querySelectorAll(
+    'div[data-visualcompletion="ignore-dynamic"]'
+  );
   for (let i = 1; i < data.length; i++) {
     var anchorTag = data[i].getElementsByTagName("a")[0];
-    const spanElements = anchorTag?.querySelectorAll('span');
-    if(spanElements)
-    {
+    const spanElements = anchorTag?.querySelectorAll("span");
+    if (spanElements) {
       const url = anchorTag.getAttribute("href");
       const name = spanElements[2].innerText;
-      const mutual = spanElements.length==6 ? spanElements[5].innerText : "No mutual friends";
-      friends.push({name, url, mutual});
+      const mutual =
+        spanElements.length == 6
+          ? spanElements[5].innerText
+          : "No mutual friends";
+      friends.push({ name, url, mutual });
     }
-    }
-    console.log("friends Data",friends);
+  }
+  console.log("friends Data", friends);
   return friends;
 }
-
